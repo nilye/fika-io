f4314825a617401d4da67e00775715ffd45a803f
nil <nil@hashbee.com>
2019-03-29 19:06:40 +0800

    add stylus

---
+++ src/page/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button class="btn pri add-to-chrome">
+      <span>C</span>
+      <span style="flex:1">Add to Chrome</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+}
+</script>
+
+<style lang="stylus">
+  .add-to-chrome
+    height 64px
+    font-size: 24px
+    padding: 0 32px
+    width 320px
+</style>
---
+++ src/page/test.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<button class="btn">Test</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'test'
+	}
+</script>
+
+<style scoped>
+</style>
